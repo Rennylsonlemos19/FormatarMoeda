cee0cfba98a751b8ee90240717a08b886f36f4a5
rennylson-lemos <renylsonlemos4321@gmail.com>
2021-11-26 19:53:09 -0300

    Projeto final

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_formatacaomoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("imforme um valor americano para ser formatado : ");
+            double valor = double.Parse(Console.ReadLine());
+
+            //moeda brasileira
+            Console.Write("\n\nvalor formatado " + valor.ToString("c"));
+
+            //moeda japonesa
+            Console.Write("\n\nvalor formatado " + valor.ToString("c", System.Globalization.CultureInfo.GetCultureInfo("ja-JP")));
+
+            //moeda americana
+            Console.Write("\n\nvalor formatado " + valor.ToString("c",System.Globalization.CultureInfo.GetCultureInfo("en-US")));
+
+            //moeda argentina
+            Console.Write("\n\nvalor formatado " + valor.ToString("c",System.Globalization.CultureInfo.GetCultureInfo("es-AG")));
+
+            Console.ReadKey();
+        }
+    }
+}
